eff003cb9423dd645321501f0361a4538607abe4
ShchurovIKM725a <Bohdan.Shchurov@infiz.khpi.edu.ua>
2026-03-05 09:25:18 +0200

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -2,10 +2,8 @@
 //
 
 #include <iostream>
-//variant3
-#include <iostream>
 using namespace std;
-
+//variant3
 int main() {
     double a, b, c, d, s;
 
@@ -34,4 +32,5 @@ int main() {
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
 
